2b36868edd32765564a79a104bca269f9e170dc2
bernardossmith <67247201+bernardossmith@users.noreply.github.com>
2020-06-23 11:43:50 -0300

    Create classes_subject.py

---
+++ classes_subject.py
@@ -0,0 +1,11 @@
+class Filhos:
+    def __init__(self,homens):
+        self.homens = homens
+
+
+benjamim = Filhos(homens="velho")
+lorenzo = Filhos(homens= "meio")
+thomaz = Filhos(homens="novo")
+print(benjamim.homens)
+print(lorenzo.homens)
+print(thomaz.homens)
